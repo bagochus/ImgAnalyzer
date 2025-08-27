61c4e101cd4c2ba73844a88a3b34836278a9905c
Евгений Багочюс <bagochus.e@gmail.com>
2025-08-27 19:26:21 +0700

    work 270825

---
+++ 2D/Calculations/PhaseRange.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImgAnalyzer._2D
+
+{
+    internal class PhaseRange : Calculation2D
+    {
+
+        int sortedArrayCount = 0;
+        double percentile;
+        double[] max_values;
+        double[] min_values;
+        int max_counter = 0;
+        int min_counter = 0;
+        Func<int, int, double> GetMaxData;
+        Func<int, int, double> GetMinData;
+
+
+        public PhaseRange()
+        {
+            this.description =
+                "Рассчитывает фазовый диапазон, реализуемый на p% \n" +
+                "площади матрицы. min, max - карты фаз. Выводит карту участков\n" +
+                "матрицы, реализующий данный фазовый диапазон";
+            this.containerNames = new string[] { "Min", "Max" };
+            this.singeValueNames = new string[] { "p" };
+            this.pixbypix = false;
+            //this.nonReturning = true;
+            
+
+        }
+
+        public override double Measure(int x, int y) 
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public override double[,] MeasureFull()
+        {
+            double[,] result = new double[width, height];
+            percentile = SingleValueParameters[0];
+            sortedArrayCount = (int)((1 - percentile / 100 ) * width * height);
+            max_values = new double[sortedArrayCount];
+            min_values = new double[sortedArrayCount];
+            GetMinData = ContainerParameters[0].ddata;
+            GetMaxData = ContainerParameters[1].ddata;
+
+
+            for (int i = 0; i < sortedArrayCount; i++) 
+            {
+                max_values[i] = Double.MinValue;
+                min_values[i] = Double.MaxValue;
+            }
+            for (int i = 0; i< width; i++) 
+                for (int j = 0; j<height;j++)
+                {
+                     ArrangeMaxValue(GetMinData(i, j));
+                    ArrangeMinValue(GetMaxData(i, j));
+                }
+            double bottom = max_values[sortedArrayCount -1];
+            double top = min_values[sortedArrayCount -1];
+            double range = top- bottom;
+
+
+            for (int i = 0; i < width; i++)
+                for (int j = 0; j < height; j++)
+                {
+                    if (GetMinData(i, j) <= bottom && GetMaxData(i, j) >= top)
+                        result[i, j] = 1; 
+                    else result[i, j] = 0;
+                }
+
+            MessageBox.Show("Нижняя граница:\t" + bottom.ToString("F3") + " град. / " +
+                (bottom / 180 ).ToString("F3") + "П\n" +
+                "Верхняя граница:\t" + top.ToString("F3") + " град. / " +
+                (top / 180 ).ToString("F3") + "П\n" +
+                "Диапазон:\t" + range.ToString("F3") + " град. / " +
+                (range / 180 ).ToString("F3") + "П");
+            return result;
+        }
+
+
+        private void ArrangeMinValue(double value)
+        {
+            int insertIndex = -1;
+            for (int i=0; i<sortedArrayCount; i++)
+            {
+                if (value < min_values[i]) { insertIndex = i; break; }
+            }
+            if (insertIndex == min_counter)
+            {
+                min_values[insertIndex] = value;
+                min_counter++;
+            }
+            else if (insertIndex >= 0)
+            {
+                for (int i = sortedArrayCount - 1; i > insertIndex; i--)
+                {
+                    min_values[i] = min_values[i - 1];
+                }
+                min_values[insertIndex] = value;
+                min_counter++;
+            }
+        }
+
+        private void ArrangeMaxValue(double value)
+        {
+            int insertIndex = -1;
+            for (int i = 0; i < sortedArrayCount; i++)
+            {
+                if (value > max_values[i]) { insertIndex = i; break; }
+            }
+            if (insertIndex == max_counter)
+            {
+                max_values[insertIndex] = value;
+                max_counter++;
+            }
+            else if (insertIndex >= 0)
+            {
+                for (int i = sortedArrayCount - 1; i > insertIndex; i--)
+                {
+                    max_values[i] = max_values[i - 1];
+                }
+                max_values[insertIndex] = value;
+                max_counter++;
+            }
+        }
+
+
+
+        public override void Process()
+        {
+             
+
+
+            int step = (int)SingleValueParameters[0];
+            int xcount = width / step;
+            int ycount = height / step;
+
+
+            List<double> dx = new List<double>();
+            List<double> dy = new List<double>();
+
+            double[] datax = new double[xcount * ycount];
+            double[] datay = new double[xcount * ycount];
+
+            for (int i = 0; i < xcount; i++) 
+                for (int j = 0; j < ycount; j++)
+                {
+                    //i,j - grid indicies
+                    //x,y - container indicies
+                    int x = i * step;
+                    int y = j * step;
+                    if (ContainerParameters[0].ddata(x, y) != 0 && ContainerParameters[1].ddata(x, y) != 0)
+                    {
+                        dx.Add(ContainerParameters[0].ddata(x, y));
+                        dy.Add(ContainerParameters[1].ddata(x, y));
+                    }
+
+
+
+                }
+
+            PlotForm form = new PlotForm(ContainerParameters[0].Name);
+            form.Text ="Plot: "+ ContainerParameters[0].Name + " vs " + ContainerParameters[1].Name;
+            form.AddDataMarkers(ContainerParameters[0].Name, dx.ToArray(), dy.ToArray());
+            form.Show();
+
+
+
+        }
+
+
+
+
+
+    }
+}
---
+++ 2D/GroupOperations/LUT.cs
@@ -0,0 +1,339 @@
+﻿using ScottPlot.Colormaps;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImgAnalyzer._2D.GroupOperations
+{
+    public class LU_Table : IGroupOperation
+    {
+        //-------------interface properties-----------------------------
+        public string Description
+        {
+            get
+            {
+                return 
+                    "Создает LUТ таблицу 64*64*256 на основе пакета данных\n" +
+                    "OUT0-256 целевые выходные значения\n" +
+                    "Vstart, Vstep - начальные значения и шаг АЦП соотв. кадрам\n" +
+                    "Mask - битовое поле с дефектными пикселями";
+            }
+        }
+
+        public double[] SingleValueParameters { get; set; }
+
+        string[] singeValueNames = new string[] { "OUT0", "OUT255", "Vstart", "Vstep" };
+        public string[] SingleValueNames { get { return singeValueNames; } }
+
+        public IContainer_2D[] ContainerParameters { get; set; }
+
+
+
+        public string[] ContainerNames { get {
+                if (useMask)
+                    return new string[] { "Mask" };
+                else return new string[0]; 
+            } }
+
+        public IImageSource[] imageSources { get; set; }
+
+        private string[] imgSourceNames = { "Input (phase)" };
+        public string[] imageSourceNames { get { return imgSourceNames; } }
+
+        public bool UseTransformation { get; set; }
+
+        //-------------local----------------------------------------------
+
+        string error_message = "";
+        int width, height;
+
+        double[,] avgField, avgFieldPrev;
+        int[,,] lut;
+
+        double out0, out255, vstart, vstep;
+        Func<int, int, double> GetData;
+        Func<int, int, double> GetMaskData;
+
+
+        int lut_width = 64;
+        int lut_height = 64;
+        int lut_depth = 256;
+        int max_code = 4096;
+
+        int block_width;
+        int block_height;
+
+        protected bool useMask = false;
+
+        public async Task Execute()
+        {
+            if (!Check())
+            {
+                MessageBox.Show(error_message); return;
+            }
+
+
+            out0 = SingleValueParameters[0];
+            out255 = SingleValueParameters[1];
+            vstart = SingleValueParameters[2];
+            vstep = SingleValueParameters[3];
+            width = imageSources[0].Width;
+            height = imageSources[0].Height;
+
+            block_width = (int)Math.Ceiling((double)width / lut_width);
+            block_height = (int)Math.Ceiling((double)height / lut_height);
+
+            avgField = new double[lut_width, lut_height];
+            avgFieldPrev = new double[lut_width, lut_height];
+            lut = new int[lut_width, lut_height, lut_depth];
+
+            for (int i = 0; i < lut_width; i++)
+                for (int j = 0; j < lut_height; j++)
+                    for (int k = 0; k < lut_depth; k++)
+                        lut[i, j, k] = -1;
+
+
+            if (useMask)
+            {
+                GetMaskData = ContainerParameters[0].ddata;
+            }
+            else
+            {
+                GetMaskData = (x, y) => 1;
+            }
+
+
+
+            ContainerBatch batch = new ContainerBatch();
+            batch.Name = ImageManager.GetUniqueSourceName("LUT");
+            ImageManager.containerBatches.Add(batch);
+
+            DataManager_2D.workToBeDone += imageSources[0].Count;
+
+            string foldername = FileManagement.CreateUniqueFolder("D:\\containers\\" + batch.Name);
+
+
+
+            await Task.Run(() =>
+            {
+                SweepInputData();
+                double[,] lut_data = new double[lut_width, lut_height];
+                for (int lut_layer = 0; lut_layer < lut_depth;lut_layer++)
+                {
+                    for (int i = 0;i < lut_width;i++)
+                        for (int j = 0;j < lut_height;j++)
+                            lut_data[i,j] = lut[i,j,lut_layer];
+
+                    Container_2D_double c = new Container_2D_double(lut_data);
+                    DataManager_2D.progress.Report(1);
+
+                    string filename = Path.Combine(foldername, lut_layer.ToString() + ".bin");
+                    c.SaveToFile(filename);
+                    batch.Filenames.Add(filename);
+                }
+
+
+
+            });
+
+
+            
+
+        }
+
+        public bool Check()
+        {
+            bool result = true;
+            if (useMask)
+            {
+                result &= (ContainerParameters[0].Width == imageSources[0].Width);
+                result &= (ContainerParameters[0].Height == imageSources[0].Height);
+            }
+
+            if(!result)
+            {
+                error_message = "Размеры полей не совпадают";
+                return false;
+
+            }
+
+            return true;
+        }
+
+
+        
+        private void SweepInputData()
+        {
+            I2DFileHandler hndl_first = imageSources[0].Get2DFileHandler(0);
+            GetData = hndl_first.GetPixelValue;
+            GenerateAverageField();
+            avgFieldPrev = avgField;
+            hndl_first.Dispose();
+
+            for (int i = 1; i < imageSources[0].Count; i++)
+            {
+                I2DFileHandler hndl = imageSources[0].Get2DFileHandler(i);
+                GetData = hndl.GetPixelValue;
+                GenerateAverageField();
+                AnalyzeFields(i - 1);
+                avgFieldPrev = avgField;
+                hndl.Dispose();
+            }
+        }
+
+        private void AnalyzeFields(int n)
+        {
+            //n - номер кадра, из которого получено avgFieldPrev
+            //пробегаем по значениям двух усреденнных полей, ищем, можем ли мы что нибудь внести в ЛУТ
+            for (int i = 0; i < lut_width; i++)
+                for (int j = 0; j < lut_height; j++)
+                {
+                    FindCodes(avgFieldPrev[i, j], avgField[i, j],n,i,j);
+
+                }
+        }
+        
+        private void GenerateAverageField()
+        {
+            avgField = new double[lut_width, lut_height];
+            for (int i = 0; i <lut_width;i++)
+                for (int j = 0; j< lut_height; j++)
+                {
+                    avgField[i,j] = CalculateBlockValue(i, j);
+                }
+        }
+
+        private double CalculateBlockValue(int i_block, int j_block)
+        {
+            double result = 0;
+            int count = 0;
+            for (int i = 0; i < block_width; i++)
+            {
+                int x = i_block * block_width + i;
+                if (x > width - 1) continue;
+
+                for (int j = 0; j < block_height; j++)
+                {
+
+                    int y = j_block * block_height + j;
+                    if (y > height - 1) continue;
+                    if (GetMaskData(x,y) == 0) continue;
+                    result += GetData(x, y);
+                    count++;
+                }
+            }
+            if (count == 0) return 0;
+            else return result/count;
+
+        }
+
+        private void FindCodes(double value1, double value2, int v_index,int lut_i,int lut_j)
+        {
+            //value1, value2 - измеренные значения "фазы" для двух соседних точек
+            //v_index - номер кадра, при котором было получено значение value1
+            double step = (out255 - out0) / (lut_depth - 1);
+            //int lut_index = -1;
+            //пробегаем по всему требуемому диапазону фаз
+            for (int i = 0; i < lut_depth; i++)
+            {
+                //выбираем одно из целевых значений фазы
+                double value = out0 + step * i;
+
+                double low_value = Math.Min(value1, value2);
+                double high_value = Math.Max(value1, value2);
+
+                if (value >= low_value && value < high_value)
+                {
+                    //если целевое значение оказалось между нашими полученными
+                    //то возвращаем взвешенный код АЦП который будет находится между v_index и v_index+1
+                    double float_code = vstart + vstep * (v_index + (value - value1) / (value2 - value1));
+                    //защита от дурака
+                    if (float_code < 0) float_code = 0;
+                    if (float_code > max_code) float_code = max_code;
+                    //Записываем в ЛУТ наш код
+                    
+                    
+
+                    lut[lut_i, lut_j,i] = (int)Math.Round(float_code);
+                }
+            }
+
+
+        }
+        
+        private void PatchTable()
+        {
+            int[,] correctValues = new int[lut_width,lut_height];
+
+            for (int i = 0; i < lut_width; i++) 
+                for (int j = 0; j < lut_height; j++)
+                {
+                    correctValues[i,j] = 0;
+                    for (int k = 0; k < lut_depth; k++)
+                        if (lut[i, j, k] != -1) correctValues[i, j]++;
+                }
+
+
+
+
+
+
+        }
+
+        private void PatchOneValuePilar(int i, int j)
+        {
+            int value = 0;
+            for (int k = 0; k < lut_depth; k++)
+            {
+                if (lut[i,j,k] != -1)
+                {
+                    value = lut[i,j,k];
+                    break;
+                }
+            }
+            for (int k = 0; k < lut_depth; k++) lut[i,j,k] = value;
+
+        }
+
+        private void PatchFewValuesPilar(int i, int j)
+        {
+            int[] patched_array = new int[lut_depth];
+            int good_index1 = -1;
+            int good_index2 = -1;
+            int good_value1 = 0;
+            int good_value2 = 0;
+
+            bool need_head_patch = false;
+            bool need_tail_patch = false;
+
+            for (int k = 0; k < lut_depth; k++)
+            {
+                if (lut[i,j,k] != -1)
+                {
+                    patched_array[k] = lut[i,j,k];
+                    if (k == 0 || good_index1 == -1) good_index1 = k;
+                    else
+                    {
+                        
+                    }
+                }
+
+
+            }
+
+
+
+
+
+        }
+
+
+
+
+    }
+}
